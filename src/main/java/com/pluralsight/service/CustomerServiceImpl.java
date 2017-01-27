8340ce9dca5aec0546c7e6c7b726909ed9b69bd9
markartur <mario.dornelles@gmail.com>
2017-01-27 00:55:45 -0200

    Wiring init

--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -4,17 +4,19 @@ import java.util.List;
 
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
-import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
 
 public class CustomerServiceImpl implements CustomerService {
-	private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+	private CustomerRepository customerRepository;
 
-	/* (non-Javadoc)
-	 * @see com.pluralsight.service.CustomerInterface#findAll()
-	 */
+
 	@Override
 	public List<Customer> findAll() {
 		return customerRepository.findAll();
+	}
+
+
+	public void setCustomerRepository(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
 	}
 
 }
